511b291f7bdc1f5cf927ae011061b62cbc2a5647
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2024-04-05 06:21:39 +0200

    added .envrc, extended .gitignore and flake

--- flake.nix
+++ flake.nix
@@ -13,11 +13,12 @@
       withPkgsFor = fn: nixpkgs.lib.genAttrs (builtins.attrNames hyprland.packages) (system: fn system nixpkgs.legacyPackages.${system});
     in
     {
-      packages = withPkgsFor (system: pkgs: {
+      packages = withPkgsFor (system: pkgs: rec {
         hycov = pkgs.callPackage ./default.nix {
           inherit (hyprland.packages.${system}) hyprland;
           stdenv = pkgs.gcc13Stdenv;
         };
+        default = hycov;
       });
 
       devShells = withPkgsFor (system: pkgs: {
